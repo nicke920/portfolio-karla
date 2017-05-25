c6f19026ce7c59b442624c59e1a2765f2171b0e7
Nick Evans <evans2nick@gmail.com>
2017-05-24 20:36:33 -0400

    second commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,55 @@
 const gulp = require('gulp');
+const babel = require('babelify');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
-const babel = require('gulp-babel');
-const autoprefixer = require('gulp-autoprefixer');
-const browserSync = require('browser-sync').create();
+const buffer = require('vinyl-buffer');
+const browserSync = require('browser-sync');
 const reload = browserSync.reload;
+const autoprefixer = require('gulp-autoprefixer');
+const notify = require('gulp-notify');
 const plumber = require('gulp-plumber');
+const historyApiFallback = require('connect-history-api-fallback');
 
-gulp.task('styles', () => {
-	return gulp.src('./dev/styles/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer('last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-		.pipe(concat('style.css'))
-		.pipe(gulp.dest('./public/styles/'));
+gulp.task('js', () => {
+    browserify('dev/scripts/main.js')
+        .transform('babelify', {
+            presets: ['es2015','react']
+        })
+        .bundle()
+        .on('error',notify.onError({
+            message: "Error: <%= error.message %>",
+            title: 'Error in JS 💀'
+        }))
+        .pipe(source('main.js'))
+        .pipe(buffer())
+        .pipe(gulp.dest('public/scripts/'))
+        .pipe(reload({stream:true}));
 });
 
-gulp.task("scripts", () =>{
-	return gulp.src("./dev/scripts/main.js")
-		.pipe(plumber())
-		.pipe(babel({
-			presets: ['es2015']
-		}))
-		.pipe(gulp.dest("./public/scripts"))
-		.pipe(reload({stream: true}));
+gulp.task('bs', () => {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }, 
+        middleware: [historyApiFallback()]
+    });
 });
 
-gulp.task("watch", () => {
-	gulp.watch("./dev/styles/**/*.scss", ['styles']);
-	gulp.watch("./dev/scripts/main.js", ["scripts"]);
-	gulp.watch('*.html', reload);
+gulp.task("styles", () => {
+   return gulp.src("./dev/styles/**/*.scss")
+   .pipe(sass().on("error",sass.logError))
+   .pipe(autoprefixer('last 5 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+   .pipe(concat("style.css"))
+   .pipe(plumber())
+   .pipe(gulp.dest("./public/styles"))
+   .pipe(reload({stream: true}));
 });
 
-gulp.task('browser-sync', () => {
-	browserSync.init({
-		server: '.'  
-	})
-});
 
-gulp.task('default', ['browser-sync', 'styles', 'scripts', 'watch']);
+gulp.task('default', ['js','bs', 'styles'], () => {
+    gulp.watch('dev/**/*.js',['js']);
+    gulp.watch('./public/style.css',reload);
+    gulp.watch('./dev/styles/*.scss', ['styles']);
+});
